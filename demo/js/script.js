9353c34eb86b43c2482cd716e167502f1fe0ef8b
FerdousiHaque <ferdousi160228@gmail.com>
2021-01-04 20:59:55 +0600

    product image hover updated

--- demo/js/script.js
+++ demo/js/script.js
@@ -127,3 +127,83 @@ ScrollTrigger.create({
 
 // make the right edge "stick" to the scroll bar. force3D: true improves performance
 gsap.set(".skewElem", {transformOrigin: "right center", force3D: true});
+
+
+//product image mouse over movement 
+var wraps = document.querySelectorAll(".movement");
+
+for (var i = 0; i < wraps.length; i++) {
+    wraps[i].addEventListener("mouseover", function () {
+        animation(this);
+    });
+}
+
+// Mouse
+var mouse = {
+    _x: 0,
+    _y: 0,
+    x: 0,
+    y: 0,
+    updatePosition: function (event) {
+        var e = event || window.event;
+        this.x = e.clientX - this._x;
+        this.y = (e.clientY - this._y) * -1;
+    },
+    setOrigin: function (e) {
+        this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
+        this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
+    },
+    show: function () {
+        return "(" + this.x + ", " + this.y + ")";
+    }
+};
+
+function animation(container) {
+
+    var inner = container.querySelector(".product");
+
+    // Track the mouse position relative to the center of the container.
+    mouse.setOrigin(container);
+    var counter = 0;
+    var refreshRate = 10;
+    var isTimeToUpdate = function () {
+        return counter++ % refreshRate === 0;
+    };
+    var onMouseEnterHandler = function (event) {
+     
+        update(event);
+    
+    };
+
+    var onMouseLeaveHandler = function () {
+        inner.style = "";
+    };
+
+    var onMouseMoveHandler = function (event) {
+        if (isTimeToUpdate()) {
+            update(event);
+        }
+    };
+    var update = function (event) {
+        mouse.updatePosition(event);
+
+        updateTransformStyle(
+            (mouse.y / inner.offsetHeight / 2).toFixed(2),
+            (mouse.x / inner.offsetWidth / 2).toFixed(2)
+        );
+    };
+
+    var updateTransformStyle = function (x, y) {
+        x = (6-x).toFixed(2);
+        var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
+        inner.style.transform = style;
+        inner.style.webkitTransform = style;
+        inner.style.mozTranform = style;
+        inner.style.msTransform = style;
+        inner.style.oTransform = style;
+    };
+    container.onmousemove = onMouseMoveHandler;
+    container.onmouseleave = onMouseLeaveHandler;
+    container.onmouseenter = onMouseEnterHandler;
+
+}
